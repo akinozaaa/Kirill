f9fcee324d6ebb339245146d4a63f362eb363882
akinozaaa <144803294+akinozaaa@users.noreply.github.com>
2023-09-12 16:12:05 +0300

    Add files via upload

---
+++ tmnt.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:3306
+-- Время создания: Сен 12 2023 г., 13:11
+-- Версия сервера: 5.6.34-log
+-- Версия PHP: 7.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `tmnt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `fractions`
+--
+
+CREATE TABLE `fractions` (
+  `id` int(11) NOT NULL,
+  `fraction` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `person`
+--
+
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Age` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `race`
+--
+
+CREATE TABLE `race` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `world_of_origin` varchar(255) NOT NULL,
+  `peculiarities` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `Login` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `weapons`
+--
+
+CREATE TABLE `weapons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `type_of_damage` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `world`
+--
+
+CREATE TABLE `world` (
+  `name` varchar(255) NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `season_of_emergence` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `fractions`
+--
+ALTER TABLE `fractions`
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `person`
+--
+ALTER TABLE `person`
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `race`
+--
+ALTER TABLE `race`
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Индексы таблицы `weapons`
+--
+ALTER TABLE `weapons`
+  ADD KEY `id` (`id`);
+
+--
+-- Индексы таблицы `world`
+--
+ALTER TABLE `world`
+  ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `fractions`
+--
+ALTER TABLE `fractions`
+  ADD CONSTRAINT `fractions_ibfk_1` FOREIGN KEY (`id`) REFERENCES `person` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `race`
+--
+ALTER TABLE `race`
+  ADD CONSTRAINT `race_ibfk_1` FOREIGN KEY (`id`) REFERENCES `person` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `weapons`
+--
+ALTER TABLE `weapons`
+  ADD CONSTRAINT `weapons_ibfk_1` FOREIGN KEY (`id`) REFERENCES `person` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `world`
+--
+ALTER TABLE `world`
+  ADD CONSTRAINT `world_ibfk_1` FOREIGN KEY (`id`) REFERENCES `person` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
